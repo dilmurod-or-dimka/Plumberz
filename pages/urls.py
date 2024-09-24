c9decd2cf1ee6c3b538b44b87b95002114c64d20
user <user@kali.user>
2024-09-24 16:37:25 +0500

    add pages

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.home_view, name="home"),
+    path("about/", views.about_view, name="about"),
+    path("service/", views.service_view, name="service"),
+    path("contact/", views.contact_view, name="contact"),
+    path("booking/", views.booking_view, name="booking"),
+    path("team/", views.team_view, name="team"),
+    path("testimonial/", views.testimonial_view, name="testimonial"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -6,4 +6,22 @@ from django.shortcuts import render, HttpResponse
 
 
 def home_view(request):
-    return HttpResponse("MIRZO GANDON")
+    return render(request, "pages/index.html")
+
+def about_view(request):
+    return render(request, "pages/about.html")
+
+def service_view(request):
+    return render(request, "pages/service.html")
+
+def contact_view(request):
+    return render(request, "pages/contact.html")
+
+def booking_view(request):
+    return render(request, "pages/booking.html")
+
+def team_view(request):
+    return render(request, "pages/team.html")
+
+def testimonial_view(request):
+    return render(request, "pages/testimonial.html")
